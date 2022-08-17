1a6ec59bfe0fd4ec99966155ea7a8888ee06a847
I0HuKc <web-disain@mail.ua>
2022-08-17 16:18:32 +0300

    Расширил трейт `ReturnTrait`

--- cypher/src/query/return_query.rs
+++ cypher/src/query/return_query.rs
@@ -1,9 +1,120 @@
-use super::execute::{Execute, ExecuteTrait};
+use crate::query::finalize::{Finalize, FinalizeTrait};
+
+pub trait SkipControlTrait: 'static + FinalizeTrait {
+    fn skip(&self, value: usize) -> Box<dyn FinalizeTrait>;
+}
+
+pub struct SkipControlQuery(String);
+
+impl SkipControlTrait for SkipControlQuery {
+    fn skip(&self, value: usize) -> Box<dyn FinalizeTrait> {
+        let state = format!(
+            "{prev_state}\nSKIP {value}",
+            prev_state = self.0,
+            value = value
+        );
+        Box::new(Finalize(state))
+    }
+}
+
+impl FinalizeTrait for SkipControlQuery {
+    fn finalize(&self) -> String {
+        self.0.clone()
+    }
+}
+
+pub trait LimitControlTrait: 'static + SkipControlTrait + FinalizeTrait {
+    fn limit(&self, value: usize) -> Box<dyn SkipControlTrait>;
+}
+
+pub struct LimitControlQuery(String);
+
+impl SkipControlTrait for LimitControlQuery {
+    fn skip(&self, value: usize) -> Box<dyn FinalizeTrait> {
+        let state = format!(
+            "{prev_state}\nSKIP {value}",
+            prev_state = self.0,
+            value = value
+        );
+        Box::new(Finalize(state))
+    }
+}
+
+impl LimitControlTrait for LimitControlQuery {
+    fn limit(&self, value: usize) -> Box<dyn SkipControlTrait> {
+        let state = format!(
+            "{prev_state}\nLIMIT {value}",
+            prev_state = self.0,
+            value = value
+        );
+        Box::new(SkipControlQuery(state))
+    }
+}
+
+impl FinalizeTrait for LimitControlQuery {
+    fn finalize(&self) -> String {
+        self.0.clone()
+    }
+}
+
+pub trait ReturnParamTrait: 'static + LimitControlTrait + FinalizeTrait {
+    fn r#as(&self, r#as: &str) -> Box<dyn LimitControlTrait>;
+}
+
+pub struct ReturnParamQuery {
+    nv: String,
+    state: String,
+}
+
+impl ReturnParamQuery {
+    pub fn new(nv: String, state: String) -> Self {
+        Self { nv, state }
+    }
+}
+
+impl LimitControlTrait for ReturnParamQuery {
+    fn limit(&self, value: usize) -> Box<dyn SkipControlTrait> {
+        let state = format!(
+            "{prev_state}\nLIMIT {value}",
+            prev_state = self.state,
+            value = value
+        );
+        Box::new(SkipControlQuery(state))
+    }
+}
+
+impl SkipControlTrait for ReturnParamQuery {
+    fn skip(&self, value: usize) -> Box<dyn FinalizeTrait> {
+        let state = format!(
+            "{prev_state}\nSKIP {value}",
+            prev_state = self.state,
+            value = value
+        );
+        Box::new(Finalize(state))
+    }
+}
+
+impl ReturnParamTrait for ReturnParamQuery {
+    fn r#as(&self, r#as: &str) -> Box<dyn LimitControlTrait> {
+        let state = format!(
+            "{prev_state} {node_var} AS {as_val}",
+            prev_state = self.state,
+            node_var = self.nv,
+            as_val = r#as
+        );
+        Box::new(LimitControlQuery(state))
+    }
+}
+
+impl FinalizeTrait for ReturnParamQuery {
+    fn finalize(&self) -> String {
+        self.state.clone()
+    }
+}
 
 pub trait ReturnTrait: 'static {
-    fn r#return(&mut self) -> Box<dyn ExecuteTrait>;
-    fn return_as(&mut self, r#as: &str) -> Box<dyn ExecuteTrait>;
-    fn return_field(&mut self, field: &str) -> Box<dyn ExecuteTrait>;
+    fn r#return(&mut self) -> Box<dyn ReturnParamTrait>;
+    fn return_field(&mut self, field: &str) -> Box<dyn FinalizeTrait>;
 }
 
 pub struct ReturnQuery {
@@ -18,18 +129,22 @@ impl ReturnQuery {
 }
 
 impl ReturnTrait for ReturnQuery {
-    fn r#return(&mut self) -> Box<dyn ExecuteTrait> {
-        let state = format!("{}\nRETURN {}", self.state, self.nv);
-        Box::new(Execute(state))
+    fn r#return(&mut self) -> Box<dyn ReturnParamTrait> {
+        let state = format!(
+            "{prev_state}\nRETURN {node_var}",
+            prev_state = self.state,
+            node_var = self.nv
+        );
+        Box::new(ReturnParamQuery::new(self.nv.clone(), state))
     }
 
-    fn return_as(&mut self, r#as: &str) -> Box<dyn ExecuteTrait> {
-        let state = format!("{}\nRETURN {} AS {}", self.state, self.nv, r#as);
-        Box::new(Execute(state))
-    }
-
-    fn return_field(&mut self, field: &str) -> Box<dyn ExecuteTrait> {
-        let state = format!("{}\nRETURN {}.{}", self.state, self.nv, field);
-        Box::new(Execute(state))
+    fn return_field(&mut self, field: &str) -> Box<dyn FinalizeTrait> {
+        let state = format!(
+            "{prev_state}\nRETURN {node_var}.{prop_name}",
+            prev_state = self.state,
+            node_var = self.nv,
+            prop_name = field
+        );
+        Box::new(Finalize(state))
     }
 }
